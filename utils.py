b559ca616bb9fe88cf4bdd57777ea86e90af4acb
Mike <moc@caltech.edu>
2020-02-12 14:02:18 -0800

    load training data as a torch Dataset

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
+# Import standard library modules
 import zipfile
-import numpy as np
 from warnings import warn
+# Import third-party modules
+import numpy as np
+import torch
+# Import custom modules
+
 
 def load_train_data(datafolder='./data/', skiprows=1, shuffle=False):
     ''' Try running this command
@@ -18,6 +23,23 @@ def load_test_data(datafolder='./data/', skiprows=1, shuffle=False):
             Xtest = utils.load_test_data()
     '''
     return np.genfromtxt(datafolder + 'test.csv', skip_header=skiprows, delimiter=',')
+
+def load_train_Dataset(*args, **kwargs):
+    ''' Functions similar to load_train_data(), but instead returns a torch Dataset object.
+        Try running
+            train_dataset = utils.load_train_Dataset()
+    '''
+    dataset = torch.utils.data.TensorDataset(load_test_data(**kwargs))
+    return dataset
+
+# def load_test_Dataset(*args, **kwargs):
+#     ''' Functions similar to load_train_data(), but instead returns a torch Dataset object.
+#         Try running
+#             train_dataset = utils.load_train_Dataset()
+#     '''
+#     dataset = torch.utils.data.TensorDataset(load_test_data(**kwargs))
+#     return dataset
+    
 
 def unzip_data(datafolder='./data'):
     with zipfile.ZipFile('caltech-cs155-2020.zip', 'r') as zip_ref:
