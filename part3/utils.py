957f64abbb8c92f3ed194ebc47e06f70cb2e94ab
Mike O'Connell <moc@caltech.edu>
2020-03-26 18:53:43 -0700

    Enforce correct syllable count per line when generating sonnets with utils.serenade_me_oh_sonneteer()

--- part3/utils.py
+++ part3/utils.py
@@ -3,6 +3,8 @@ import numpy as np
 from nltk import TweetTokenizer
 tknzr = TweetTokenizer()
 
+# ENDVALUE = 'theend'
+
 def load_sonnets(datafile = './data/shakespeare.txt'):
     with open(datafile, 'r') as file:
         text = file.read()
@@ -42,7 +44,7 @@ def sonnet_to_sequence(sonnet, word_library):
         for word in line:
             sequence.append(word_library[word])
         sequence.append(word_library['\n'])
-    sequence.append(word_library['\end'])
+#     sequence.append(word_library[ENDVALUE])
     return sequence
 
 def sequence_to_sonnet(sequence, feat_library):
@@ -55,16 +57,16 @@ def sequence_to_sonnet(sequence, feat_library):
         if word == '\n':
             sonnet.append(line)
             line = []
-        elif word == '\end':
-            return sonnet
+#         elif word == ENDVALUE:
+#             return sonnet
         else:
             line.append(word)
     return sonnet
             
 def vectorize_sonnets(sonnets):
-    word_library = {'\n':0, '\end':1}
-    feat_library = {0:'\n', 1:'\end'}
-    num_features = 2
+    word_library = {'\n':0}#, ENDVALUE:1}
+    feat_library = {0:'\n'}#, 1:ENDVALUE}
+    num_features = 1#2
     for sonnet in sonnets:
         for line in sonnet:
             for word in line:
@@ -91,3 +93,55 @@ def print_sonnet(sonnet):
             string += ' '
         string += '\n'
     print(string)
+    
+def get_syllable_dict(datafile = './data/Syllable_dictionary.txt'):
+    syllable_dict = {} 
+    with open(datafile) as f:
+        for line in f:
+            l = line.split(' ')
+            word = l[0]
+            word = word.replace("'", "")
+            word = word.replace("-", "")        
+            # Check if word is already in dictionary
+            if not word in syllable_dict.keys():
+                for i in range(len(l)):
+                    if i == 0:
+                        continue
+                    try:
+                        syllable_dict[word] = int(l[i][0])
+                        break
+                    except:
+                        pass
+    return syllable_dict
+
+def count_syllables(line, syllable_dict=get_syllable_dict()):
+    syllable_count = 0
+    for word in line:
+        punc = False
+        for i in range(len(word)):
+            if word[i] in string.punctuation:
+                punc = True
+        if not punc:
+            syllable_count += syllable_dict[word]
+    return syllable_count
+
+def serenade_me_oh_sonneteer(model, feat_library):
+    seq = np.empty((0,1),int)
+    start_new_line = 0
+    for i in range(14):
+        syllable_count = 0
+        if i in [4,8,12]:
+            seq = np.vstack((seq, [[0]]))
+        while syllable_count != 10:
+            test_seq, test_states = model.sample(n_samples=20)
+            try:
+                end_line = np.asarray(test_seq[start_new_line:,0] == 0).nonzero()[0][0]
+            except:
+                continue
+            test_seq = test_seq[:end_line+1]
+            if test_seq[0].item == "\n": # explicitly call .item to avoid this warning https://stackoverflow.com/questions/40659212/futurewarning-elementwise-comparison-failed-returning-scalar-but-in-the-futur
+                continue
+            test_line = sequence_to_sonnet(test_seq, feat_library)[0]
+            syllable_count = count_syllables(test_line)
+        seq = np.vstack((seq, test_seq[:end_line+1]))
+    print_sonnet(sequence_to_sonnet(seq, feat_library))
